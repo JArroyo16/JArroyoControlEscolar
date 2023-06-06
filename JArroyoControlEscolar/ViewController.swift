a68c94633893a76399fb3239c047423784f29eb8
JArroyo16 <josephreya16@gmail.com>
2023-06-06 12:18:04 -0600

    Initial Commit

---
+++ JArroyoControlEscolar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JArroyoControlEscolar
+//
+//  Created by MacBookMBP1 on 06/06/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
